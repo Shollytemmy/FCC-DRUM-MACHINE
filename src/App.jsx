d1f0255b6522a1ac184d21e3edad4b241a10ec3c
Bunyameen <bunyameen@gmail.com>
2022-12-16 18:26:14 +0100

    Drum Machine: building the UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import './App.css'
+import './App.scss'
+import { DrumPads } from './Components/DrumPads'
+import { Display } from './Components/Display'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <h1>Drum Machine</h1>
+      
+      <DrumPads />
+      <Display />
     </div>
   )
 }
---
+++ src/Components/DrumPads.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const DrumPads = () => {
+  return (
+    <div id='drumPad' className='drumPad'>DrumPads</div>
+  )
+}
