470d8324555802072eff207b6ada90eebdf9f895
Bunyameen <bunyameen@gmail.com>
2022-12-17 13:13:59 +0100

    Drum Machine: Styling display section

--- src/Components/Display.jsx
+++ src/Components/Display.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 export const Display = ({currentSong, updateCurrent}) => {
   return (
     <div id='dispay' className='display'>
-      <p>{currentSong}</p>
+      <div className="display__text">
+        <p>{currentSong}</p>
+      </div>
     </div>
   )
 }
